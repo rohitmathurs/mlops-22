08590e9896d3f01fbabb697eda05e46915830d24
rohitmathurs <mathur.9@iitj.ac.in>
2022-09-14 09:48:30 +0530

    in-class hyperparameter tuning

--- plot_graphs.py
+++ plot_graphs.py
@@ -12,7 +12,15 @@ from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
 
 
-GAMMA = 0.001
+# Class exercise:
+# 1. Set the ranges of hyper parameters
+gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
+c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
+
+# model hyperparams
+# GAMMA = 0.001
+# C = 0.5
+
 train_frac = 0.8
 test_frac = 0.1
 dev_frac = 0.1
@@ -47,21 +55,54 @@ X_test, X_dev, y_test, y_dev = train_test_split(
 )
 
 
-#PART: Define the model
-# Create a classifier: a support vector classifier
-clf = svm.SVC()
+# 2. Train for every combination of hyperparameter values
+# 2a. Train the model
+# 2b. Compute the acuracy on the validation set
+# 3. Identify the best combination of hyperparameters for which validation set performance is maximum
+# 4. Report the test set accuracy with this best model
 
-#PART: setting up hyperparameter
-hyper_params = {'gamma':GAMMA}
-clf.set_params(**hyper_params)
+# Variable for storing accuracy of the current combination
+acc = 0
 
+#Variable for storing the current max accuracy
+max_acc = 0;
 
-#PART: Train model
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
+# Variables for storing the current best hyper_parameter combination
+best_gamma = 0
+best_c = 0
+
+for gamma in gamma_list:
+	for C in c_list:
+		
+		#PART: Define the model
+		# Create a classifier: a support vector classifier
+		clf = svm.SVC()
+		
+		#PART: setting up hyperparameter
+		hyper_params = {'gamma':gamma, 'C':C}
+		clf.set_params(**hyper_params)
+
+		#PART: Train model
+		# Learn the digits on the train subset
+		clf.fit(X_train, y_train)
+
+		#PART: Get dev set predictions
+		# Predict the value of the digit on the test subset
+		predicted = clf.predict(X_dev)
+
+		acc = metrics.accuracy_score(y_pred=predicted, y_true=y_dev)
+		
+		if(acc > max_acc):
+			max_acc = acc
+			best_gamma = gamma
+			best_c = C
 
 #PART: Get test set predictions
 # Predict the value of the digit on the test subset
+
+#PART: setting up hyperparameter
+hyper_params = {'gamma':best_gamma, 'C':best_c}
+clf.set_params(**hyper_params)
 predicted = clf.predict(X_test)
 
 #PART: Sanity check of predictions
@@ -71,9 +112,14 @@ for ax, image, prediction in zip(axes, X_test, predicted):
     image = image.reshape(8, 8)
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title(f"Prediction: {prediction}")
-
+				
 #PART: Compute evaluation metrics
 print(
-    f"Classification report for classifier {clf}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
+	f"\nClassification report for classifier {clf}:\n"
+	f"{metrics.classification_report(y_test, predicted)}\n"
 )
+
+print("\nBest gamma = ", best_gamma)
+print("\nBest C = ", best_c)
+print("\nMax accuracy = ", max_acc)
+print("\n")
