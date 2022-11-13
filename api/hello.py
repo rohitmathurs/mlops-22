e3ca43cbb1fc8b8bca4f68910f7bea4e1bd8a14c
rohitmathurs <mathur.9@iitj.ac.in>
2022-11-13 15:24:14 +0530

    images test functional

--- api/hello.py
+++ api/hello.py
@@ -2,28 +2,25 @@ from flask import Flask
 from flask import request
 from joblib import load
 
-
 app = Flask(__name__)
 
 #Load one of the saved models
 model_path = "../svm_Gamma=0.01_C=0.5.joblib"
 model = load(model_path)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, MlOps Class!</p>"
-    
-
-@app.route("/sum", methods=['POST'])
-def sum():
-	x = request.json['x']
-	y = request.json['y']
-	z = x + y
-	return {"sum":z}
 	
 @app.route("/predict", methods=['POST'])
 def predict_digit():
-	image = request.json['image']
+	image1 = request.json['image1']
+	image2 = request.json['image2']
 	print("Loaded model")
-	predicted = model.predict([image])
-	return {"y_predicted":int(predicted[0])}
+	
+	predicted = model.predict([image1])
+	image1_predicted = int(predicted[0])
+	
+	predicted = model.predict([image2])
+	image2_predicted = int(predicted[0])
+
+	if image1_predicted == image2_predicted:
+		return '\nInput images are the same digit\n\n'
+	else:
+		return '\nInput images are not the same digit\n\n'
